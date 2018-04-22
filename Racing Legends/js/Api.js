4e46852cea8b493a49a590aed2c9bbea38392864
David Moreno <aluno20165@ipt.pt>
2018-04-22 11:12:05 +0100

    css

--- Racing Legends/js/Api.js
+++ Racing Legends/js/Api.js
@@ -1,14 +1,86 @@
 ﻿document.addEventListener('DOMContentLoaded', function main(e) {
+    //**************Categorias*****************************
+
+    function mostrarCategorias(categorias) {
+        var categor = document.getElementById("categorias");
+        
+        document.body.appendChild(categor);
+        for (var i = 0; i < categorias.length; i++) {
+            var p = document.createElement('p');
+            var p1 = document.createElement('p');
+            var p2 = document.createElement('p');
+            var img = document.createElement('img');
+            p.textContent = categorias[i].name;
+            //p1.textContent = categorias[i].id;
+            p2.textContent = categorias[i].description;
+            img.src = getImg(categorias[i].id);
+            //p1.textContent = getImg(categorias[i].id);
+            categor.appendChild(p);
+            categor.appendChild(p1);
+            categor.appendChild(p2);
+            categor.appendChild(img);
+            
+        }
+        ///////////////////////
+        
+    }
+
+
+    function ecraCategorias() {
+        return getCategorias()
+            .then(function (categorias) {
+                mostrarCategorias(categorias);
+            })
+            .catch(function (erro) {
+                console.error(erro);
+            });
+    }
+
+
+    ecraCategorias();
+
+
+    //**************************************************
+
+    //*****************Pilotos**************************
+
+    function mostrarPilotos(idCategoria) {
+
+        var pilot = document.getElementById("pilotos");
+        document.body.appendChild(pilot);
+
+        for (var i = 0; i < idCategoria.length; i++) {
+            var p3 = document.createElement('p');
+            var p4 = document.createElement('p');
+            var img1 = document.createElement('img');
+            var p5 = document.createElement('p');
+            p3.textContent = idCategoria[i].name;
+            p4.textContent = idCategoria[i].nationality;
+            img1.src = getImgP(idCategoria[i].id);
+            
+            pilot.appendChild(p3);
+            pilot.appendChild(p4);
+            pilot.appendChild(img1);
+           
+        }
+
+    }
+
+    function ecraPilotos(idCategoria) {
+        return getDrivers(idCategoria)
+            .then(function (idCategoria) {
+                mostrarPilotos(idCategoria);
+            }).catch(function (erro) {
+                console.error(erro);
+            });
+            
+    }    
+
+
+    //ecraPilotos('f1');
 
 
 
 
 
-
-
-
-
-
-
-
-})
+});
--- Racing Legends/js/dados.js
+++ Racing Legends/js/dados.js
@@ -1,1 +1,40 @@
-﻿
+﻿//Ficheiro de funções de acessoa a dados
+
+//1)
+function getCategorias() {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories";
+
+    return fetch(url, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            if (resposta.status == 200) {
+                return resposta.json();
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+}
+
+function getImg(id_categoria) {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories/" + id_categoria + "/image";
+
+    return url;
+}
+
+function getDrivers(id_categoria) {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories/" + id_categoria + "/drivers";
+
+    return fetch(url, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            if (resposta.status == 200) {
+                return resposta.json();
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+}
+
+function getImgP(id_piloto) {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/drivers/" + id_piloto + "/image";
+
+    return url;
+}
